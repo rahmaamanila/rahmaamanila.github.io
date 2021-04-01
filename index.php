62946551cfa0783e8ef78e4a61098022d00f8971
rahmaamanila <rahmaamanila@gmail.com>
2021-03-31 18:33:55 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+    <title>My Portfolio | Rahma Amanila</title>
+</head>
+<body id="home">
+
+<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Rahma Amanila</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                     <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#about">About</a>
+                </li>
+                 <li class="nav-item">
+                     <a class="nav-link" href="#favorite">Favorite</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+<!-- Akhir Navbar -->
+
+<!-- Jumbotron -->
+<section class="jumbotron text-center">
+    <img src="img/gambar9.jpg" alt="orang" width="200" class="rounded-circle img-thumbnail">
+  <h1 class="display-4">Rahma Amanila</h1>
+  <p class="lead">A Child | Student</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,64L30,101.3C60,139,120,213,180,202.7C240,192,300,96,360,64C420,32,480,64,540,101.3C600,139,660,181,720,176C780,171,840,117,900,90.7C960,64,1020,64,1080,85.3C1140,107,1200,149,1260,149.3C1320,149,1380,107,1410,85.3L1440,64L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir Jumbotron -->
+
+<!-- About -->
+<section id="about">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>About Me</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center fs-5 text-center">
+            <div class="col-md-4">
+                <p>Hai, Assalamualaikum. Namaku Rahma Amanila, biasa dipanggil Rahma atau Nila. Aku lahir di Kota Kebumen, di bulan April. Aku anak pertama dari dua bersaudara.</p>
+            </div>
+            <div class="col-md-4">
+                <p>Sekarang aku sekolah di SMK Informatika Utama, kelas 10. Sebelumnya aku bersekolah di SMP Utama. Cita-citaku itu seperti kebanyakan orang, yaitu menjadi orang yang Sukses di masa depan.</p>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="1" d="M0,64L30,101.3C60,139,120,213,180,202.7C240,192,300,96,360,64C420,32,480,64,540,101.3C600,139,660,181,720,176C780,171,840,117,900,90.7C960,64,1020,64,1080,85.3C1140,107,1200,149,1260,149.3C1320,149,1380,107,1410,85.3L1440,64L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir About -->
+
+<!-- Favorite -->
+<section id="favorite" style="background-color: #a2d9ff;">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>My Favorite</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/beach.jpg" class="card-img-top" alt="pantai">
+                    <div class="card-body">
+                        <p class="card-text">A calming place, but also scary.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/fantasyword.jpg" class="card-img-top" alt="dunia fantasi">
+                    <div class="card-body">
+                        <p class="card-text">An amazing place, and always dreamed of.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/kucing.jpg" class="card-img-top" alt="kucing">
+                    <div class="card-body">
+                        <p class="card-text">A furry animal that always attracts attention.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,64L30,101.3C60,139,120,213,180,202.7C240,192,300,96,360,64C420,32,480,64,540,101.3C600,139,660,181,720,176C780,171,840,117,900,90.7C960,64,1020,64,1080,85.3C1140,107,1200,149,1260,149.3C1320,149,1380,107,1410,85.3L1440,64L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir Favorite -->
+
+<!-- Contact -->
+<section id="contact">
+    <div class="container">
+        <div class="row text-center mb-3">
+            <div class="col">
+                <h2>Contact Me</h2>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama Lengkap</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" aria-describedby="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pesan" class="form-label">pesan</label>
+                        <textarea class="form-control" id="pesan" rows="3"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Kirim</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,96L30,117.3C60,139,120,181,180,176C240,171,300,117,360,117.3C420,117,480,171,540,197.3C600,224,660,224,720,197.3C780,171,840,117,900,128C960,139,1020,213,1080,213.3C1140,213,1200,139,1260,101.3C1320,64,1380,64,1410,64L1440,64L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
+</section>
+<!-- Akhir Contact -->
+
+<!-- Footer -->
+<footer class="bg-primary text-white text-center pb-4">
+    <p>Created with <i class="bi bi-suit-heart-fill text-danger"></i> by <a href="https://www.instagram.com/rhm.anl/" class="text-white fw-bold">Rahma Amanila</a></p>
+</footer>
+<!-- Akhir Footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
